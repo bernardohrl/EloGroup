8a9b165aae31bcd42560f8492442f024c5ddc13c
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-03-10 23:08:12 -0300

    Adds Media Selection Logic
    Buttons change collor and save value when clicked.

--- src/app/components/dadosPessoais/dadosPessoais.component.ts
+++ src/app/components/dadosPessoais/dadosPessoais.component.ts
@@ -14,12 +14,14 @@ import { SocialMedia } from 'src/models/socialMedia.model'
 export class DadosPessoaisComponent implements OnInit {
   public locations: Array<Location> = null;
   public socialMedias: Array<SocialMedia> = null;
+  public submited: boolean = false;
 
   public dataForm = this.fb.group({
     name: null,
     phone: null,
+    hasMedias: null,
     knownFrom: [0],
-    socialMedia: [null, Validators.required],
+    socialMediaArray: [[], Validators.required],
   });
 
 
@@ -36,22 +38,39 @@ export class DadosPessoaisComponent implements OnInit {
     console.log(this.dataForm.get('name').value)
     console.log(this.dataForm.get('phone').value)
     console.log(this.dataForm.get('knownFrom').value)
-    console.log(this.dataForm.get('socialMedia').value)
+    console.log(this.dataForm.get('hasMedias').value)
+    console.log(this.dataForm.get('socialMediaArray').value)
   }
 
   socialMediaSelected(event) {
-    var selectedMedia = event.path[1];
-    var [comment, ...medias]: Array<any> = event.path[2].childNodes;
-    
+    var button = event.path[1];
+    var selected = event.srcElement.checked;
+
     var selectedStyle: string = "background-color: var(--PRIMARY-COLOR); color: white; border-color: var(--PRIMARY-COLOR); font-weight: 500";
     var defaultStyle: string = "background-color: white; color: black; border-color: black; font-weight: 100";
+    selected ? button.setAttribute("style", selectedStyle) : button.setAttribute("style", defaultStyle);
 
-    medias.forEach(plan => {
-      plan == selectedMedia ?
-              plan.setAttribute("style", selectedStyle) : 
-              plan.setAttribute("style", defaultStyle);
-    });
+    this.toggle(event.srcElement.id)
 
     this.updated();
+  }
+
+  onSubmit() {
+    console.log("aaaa")
+    this.submited = true;
+  }
+
+
+
+
+
+
+  private toggle(media) {
+    var array = this.dataForm.get('socialMediaArray').value;
+    var index = array.indexOf(media);
+
+    if (index === -1) array.push(media);
+    else              array.splice(index, 1);
+    
   }
 }
